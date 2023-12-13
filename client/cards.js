42587be6e6cea74c580f67f6d6a1998c29aaa321
Carlos Alvarado <pt002014388@idat.edu.pe>
2023-12-13 07:27:09 -0500

    Feat: files cards, index and styles modified

--- client/cards.js
+++ client/cards.js
@@ -1,12 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Tu lógica de carga de recetas aquí
-  cargarRecetas();
-
-
-  
-
   // Configurar Swiper después de agregar las diapositivas
-  new Swiper(".mySwiper", {
+  const ofertaSwiper = new Swiper(".oferta-swiper", {
     slidesPerView: 1,
     spaceBetween: 80,
     centeredSlides: false,
@@ -33,25 +27,82 @@ document.addEventListener('DOMContentLoaded', function () {
       clickable: true,
     },
   });
+
+  const nuevoSwiper = new Swiper(".nuevo-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 80,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      991: {
+        slidesPerView: 3,
+        slidesPerGroup: 2,
+      },
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  const normalSwiper = new Swiper(".normal-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 80,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      991: {
+        slidesPerView: 3,
+        slidesPerGroup: 2,
+      },
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  // Llama a la función para cargar recetas con cada condición
+  cargarRecetasConFiltro('oferta', ofertaSwiper);
+  cargarRecetasConFiltro('nuevo', nuevoSwiper);
+  cargarRecetasConFiltro('normal', normalSwiper);
 });
 
-async function cargarRecetas() {
- 
-
-
+async function cargarRecetasConFiltro(condition, swiperInstance) {
   try {
-    const response = await fetch('http://localhost:3001/api/recipes');
+    const response = await fetch(`http://localhost:3001/api/recipes?condition=${condition}`);
     const data = await response.json();
-    console.log('data:', data);
+    console.log(`Recetas con condición ${condition}:`, data);
 
-    const recipeCardsContainer = document.querySelector('.swiper-wrapper');
+    const recipeCardsContainer = swiperInstance.el.querySelector('.swiper-wrapper');
+    recipeCardsContainer.innerHTML = ""; // Limpiar contenido anterior del carrusel
 
     data.forEach(recipe => {
       const cardContainer = document.createElement('div');
       cardContainer.className = 'swiper-slide card';
       
-      const condition  = recipe.condition;
-
       const cardImage = document.createElement('div');
       cardImage.className = 'image';
       
@@ -59,7 +110,6 @@ async function cargarRecetas() {
       image.src = recipe.image_url;
       image.alt = recipe.name;
       image.addEventListener('load', function () {
-        // Agrega la imagen al contenedor después de que se haya cargado completamente
         cardImage.appendChild(image);
       });
 
@@ -76,7 +126,6 @@ async function cargarRecetas() {
       offerPrice = parseFloat(offerPrice);
       const originalPrice = condition === 'oferta' ? parseFloat(recipe.price) : null;
 
-
       cardPrice.innerHTML = `
         <span class='offerPrice'>S/${offerPrice.toFixed(2)}</span>
         ${originalPrice ? ` <span class="original-price">${originalPrice.toFixed(2)}</span>` : ""}
@@ -85,7 +134,6 @@ async function cargarRecetas() {
       const cardDiscountBadge = document.createElement('div');
       cardDiscountBadge.className = 'discount-badge';
   
-      // Verifica si la receta está en oferta antes de mostrar el porcentaje de descuento
       if (recipe.condition === 'oferta' && recipe.discount_percentage) {
         cardDiscountBadge.innerHTML = `<span>${recipe.discount_percentage}% OFF</span>`;
         cardContainer.appendChild(cardDiscountBadge);
@@ -105,21 +153,15 @@ async function cargarRecetas() {
       recipeCardsContainer.appendChild(cardContainer);
     });
 
-    function conditionValidate(price, condition, discount_percentage){
-      let offerPrice = price;
-      if(condition === 'oferta'){
-         offerPrice = price - price * (discount_percentage / 100);
-      }
-
-      return offerPrice;
-    }
-
-    function generateRatingStars(rating) {
-      const stars = Array.from({ length: rating }, (_, index) => `<span class="fa fa-star checked" data-index="${index + 1}"></span>`).join('');
-      return stars;
-    }
-
   } catch (error) {
-    console.error('Error al cargar recetas:', error);
+    console.error(`Error al cargar recetas con condición ${condition}:`, error);
   }
+}
+
+function conditionValidate(price, condition, discount_percentage){
+  let offerPrice = price;
+  if(condition === 'oferta'){
+    offerPrice = price - price * (discount_percentage / 100);
+  }
+  return offerPrice;
 }
